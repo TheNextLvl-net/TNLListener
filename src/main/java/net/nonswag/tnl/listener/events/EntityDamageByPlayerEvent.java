ac60f89f60e699401083a9373a4d4f8091e54759
NonSwag <mrminecraft00@gmail.com>
2022-11-27 20:53:02 +0100

    added cancellable

--- src/main/java/net/nonswag/tnl/listener/events/EntityDamageByPlayerEvent.java
+++ src/main/java/net/nonswag/tnl/listener/events/EntityDamageByPlayerEvent.java
@@ -1,13 +1,17 @@
 package net.nonswag.tnl.listener.events;
 
 import lombok.Getter;
+import lombok.Setter;
 import net.nonswag.tnl.listener.api.event.PlayerEvent;
 import net.nonswag.tnl.listener.api.player.TNLPlayer;
 import org.bukkit.entity.Entity;
+import org.bukkit.event.Cancellable;
 
 @Getter
-public class EntityDamageByPlayerEvent extends PlayerEvent {
+@Setter
+public class EntityDamageByPlayerEvent extends PlayerEvent implements Cancellable {
     private final Entity entity;
+    private boolean cancelled;
 
     public EntityDamageByPlayerEvent(TNLPlayer player, Entity entity) {
         super(player);
--- src/main/java/net/nonswag/tnl/listener/events/PlayerInteractAtEntityEvent.java
+++ src/main/java/net/nonswag/tnl/listener/events/PlayerInteractAtEntityEvent.java
@@ -1,13 +1,17 @@
 package net.nonswag.tnl.listener.events;
 
 import lombok.Getter;
+import lombok.Setter;
 import net.nonswag.tnl.listener.api.event.PlayerEvent;
 import net.nonswag.tnl.listener.api.player.TNLPlayer;
 import org.bukkit.entity.Entity;
+import org.bukkit.event.Cancellable;
 
 @Getter
-public class PlayerInteractAtEntityEvent extends PlayerEvent {
+@Setter
+public class PlayerInteractAtEntityEvent extends PlayerEvent implements Cancellable {
     private final Entity entity;
+    private boolean cancelled;
 
     public PlayerInteractAtEntityEvent(TNLPlayer player, Entity entity) {
         super(player);
