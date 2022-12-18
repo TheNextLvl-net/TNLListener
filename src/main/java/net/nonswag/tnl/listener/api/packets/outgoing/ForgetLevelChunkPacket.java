69e9d9bb77501daa2274960a9c35d326d43f5b00
NonSwag <mrminecraft00@gmail.com>
2022-12-18 12:53:12 +0100

    added create(Chunk) method

--- src/main/java/net/nonswag/tnl/listener/api/packets/outgoing/ForgetLevelChunkPacket.java
+++ src/main/java/net/nonswag/tnl/listener/api/packets/outgoing/ForgetLevelChunkPacket.java
@@ -5,15 +5,19 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import net.nonswag.tnl.listener.api.mapper.Mapping;
+import org.bukkit.Chunk;
 
 @Getter
 @Setter
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 public abstract class ForgetLevelChunkPacket extends PacketBuilder {
-
     private int x, z;
 
     public static ForgetLevelChunkPacket create(int x, int z) {
         return Mapping.get().packetManager().outgoing().forgetLevelChunkPacket(x, z);
+    }
+
+    public static ForgetLevelChunkPacket create(Chunk chunk) {
+        return create(chunk.getX(), chunk.getZ());
     }
 }
